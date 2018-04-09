87f368581970d7ecbb2dc2453d5c4e26033ae40b
ulie <ulrike.schnaithmann@gmail.com>
2018-04-09 18:34:14 +0200

    LEDs added

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
 const mqtt = require("mqtt");
 const moment = require("moment");
 const client = mqtt.connect("mqtt://broker.mqtt-dashboard.com");
+var sys = require("sys");
+var exec = require("child_process").exec;
+
+
+let ready = { 
+  dishwasher: false,
+  washingmachine: false
+} 
 
 client.on("connect", () => {
   client.subscribe("weather");
   client.subscribe("/energy/income");
+  client.subscribe("/device/status");
+  client.subscribe("/device/dishwasher/ready")
+  client.subscribe("/device/washingmachine/ready")
 });
 
 /**
@@ -65,16 +76,60 @@ client.on("message", (topic, message) => {
       client.publish("/device/status", "2");
     }
   }
-
-
-});
-var sys = require("sys");
-var exec = require("child_process").exec;
-var child;
-child = exec("sshpass -p 'ZH3d3IpK' ssh pi@192.168.23.120 'python led.py'", function(error, stdout, stderr) {
-  console.log("stdout: " + stdout);
-  console.log("stderr: " + stderr);
-  if (error !== null) {
-    console.log("exec error: " + error);
+   
+  /**
+   * /DEVICE/STATUS
+   */
+  if (topic=== "/device/dishwasher/ready"){
+    const state= JSON.parse(message);
+    if (state=="1"){
+      ready.dishwasher=true;
+    }
+    
   }
+
+  if (topic=== "/device/washingmachine/ready"){
+    const state= JSON.parse(message);
+    if (state=="1"){
+      ready.washingmachine=true;
+    }
+    
+  }
+
+
+  if (topic === "/device/status" && ready.dishwasher===true){
+    const status = JSON.parse(message);
+    console.log(status);
+    if (status===2){
+      var child;
+      child = exec("sshpass -p 'ZH3d3IpK' ssh pi@192.168.23.120 'python dishwasher.py & python washingmachine.py'", function(error, stdout, stderr) {
+      if (error !== null) {
+        console.log("exec error: " + error);
+      }
+      console.log("Dishwasher and Washingmachine done");
+      client.publish("/device/dishwasher/feedback", "0");
+      client.publish("/device/washingmachine/feedback", "0");
+      ready.washingmachine=false;
+      ready.dishwasher=false;
+
+    
+    });
+    }
+    else if (status===1){
+      var child;
+      child = exec("sshpass -p 'ZH3d3IpK' ssh pi@192.168.23.120 'python dishwasher.py'", function(error, stdout, stderr) {
+      if (error !== null) {
+        console.log("exec error: " + error);
+      }
+      console.log("Dishwasher done");
+      client.publish("/device/dishwasher/feedback", "0");
+      ready.dishwasher=false;
+      
+    });
+       
+  }
+}
+
+  
+
 });
