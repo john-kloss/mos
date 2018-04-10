1f3f5597486c505c40a34d240a8bc9d445b07426
ulie <ulrike.schnaithmann@gmail.com>
2018-04-10 13:10:28 +0200

    device orders added, weatherapi call changed

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 "use strict";
 const mqtt = require("mqtt");
 const moment = require("moment");
+const request = require("request");
 const client = mqtt.connect("mqtt://broker.mqtt-dashboard.com");
 var sys = require("sys");
 var exec = require("child_process").exec;
+//var sleep = require('sleep'); 
 
 
-let ready = { 
+let ready = {
   dishwasher: false,
   washingmachine: false
-} 
+};
+
+let apiKey = "bd5e378503939ddaee76f12ad7a97608";
+let city_id = "2874545"; //Magdeburg
+let url = `http://api.openweathermap.org/data/2.5/forecast?id=${city_id}&appid=${apiKey}`;
+
+var futureWeather;
+request(url, function(err, response, body) {
+  if (err) {
+    console.log("error:", error);
+  } else {
+    let weather = JSON.parse(body);
+    futureWeather = weather.list[1].weather[0].main;
+  }
+});
 
 client.on("connect", () => {
   client.subscribe("weather");
   client.subscribe("/energy/income");
   client.subscribe("/device/status");
-  client.subscribe("/device/dishwasher/ready")
-  client.subscribe("/device/washingmachine/ready")
+  client.subscribe("/device/dishwasher/ready");
+  client.subscribe("/device/washingmachine/ready");
+  client.subscribe("/central/prediction");
 });
 
 /**
@@ -24,6 +41,7 @@ client.on("connect", () => {
  */
 
 client.on("message", (topic, message) => {
+  /** 
   if (topic === "weather") {
     const data = JSON.parse(message);
     let tendencyWindSpeed;
@@ -32,7 +50,7 @@ client.on("message", (topic, message) => {
     } else {
       tendencyWindSpeed = "increasing";
     }
-    let tendencyWeather;
+    let tendencyWeather="EMPTY";
     const currentWeather = data[0].weather[0].main;
     const futureWeather = data[1].weather[0].main;
     if (currentWeather === futureWeather) {
@@ -49,17 +67,20 @@ client.on("message", (topic, message) => {
       tendencyWindSpeed,
       tendencyWeather
     };
-    client.publish("/central/prediction", weather.toString());
+    console.log(weather.tendencyWeather.toString());
+    client.publish("/central/prediction", tendencyWeather);
   }
-
+*/
   /**
    * /ENERGY/INCOME
    */
+  let currentWeather;
   if (topic === "/energy/income") {
     const energy = JSON.parse(message);
     const hour = moment().format("HH");
 
     if (energy < 5000) {
+      currentWeather = "dark";
       console.log("dark");
       client.publish("/device/status", "0");
 
@@ -69,67 +90,117 @@ client.on("message", (topic, message) => {
       //   console.log("starting");
       // }
     } else if (energy < 9000) {
+      currentWeather = "overcast";
       client.publish("/device/status", "1");
       console.log("overcast");
     } else {
+      currentWeather = "sunny";
       console.log("sunny");
       client.publish("/device/status", "2");
     }
+
+    let tendencyWeather;
+    if (currentWeather === futureWeather) {
+      tendencyWeather = "constant";
+    } else if (currentWeather === "Clear") {
+      tendencyWeather = "less sun";
+    } else if (futureWeather === "Clear") {
+      tendencyWeather = "more sun";
+    } else {
+      tendencyWeather = "constant";
+    }
+    console.log("Tendency: " + tendencyWeather + " in the next 3 hours");
+    client.publish(
+      "/central/prediction",
+      tendencyWeather.toString() + " in the next 3 hours"
+    );
   }
-   
+
   /**
    * /DEVICE/STATUS
    */
-  if (topic=== "/device/dishwasher/ready"){
-    const state= JSON.parse(message);
-    if (state=="1"){
-      ready.dishwasher=true;
+  if (topic === "/device/dishwasher/ready") {
+    const state = JSON.parse(message);
+    if (state == "1") {
+      ready.dishwasher = true;
     }
-    
   }
 
-  if (topic=== "/device/washingmachine/ready"){
-    const state= JSON.parse(message);
-    if (state=="1"){
-      ready.washingmachine=true;
+  if (topic === "/device/washingmachine/ready") {
+    const state = JSON.parse(message);
+    if (state == "1") {
+      ready.washingmachine = true;
     }
-    
   }
 
-
-  if (topic === "/device/status" && ready.dishwasher===true){
+  if (topic === "/device/status") {
     const status = JSON.parse(message);
     console.log(status);
-    if (status===2){
+    if (
+      status === 2 &&
+      ready.dishwasher === true &&
+      ready.washingmachine === true
+    ) {
       var child;
-      child = exec("sshpass -p 'ZH3d3IpK' ssh pi@192.168.23.120 'python dishwasher.py & python washingmachine.py'", function(error, stdout, stderr) {
-      if (error !== null) {
-        console.log("exec error: " + error);
+      child = exec(
+        "sshpass -p 'ZH3d3IpK' ssh pi@192.168.23.120 'python dishwasher.py & python washingmachine.py'",
+        function(error, stdout, stderr) {
+          if (error !== null) {
+            console.log("exec error: " + error);
+          }
+          //client.publish("/device/status", "2");
+          client.publish("/device/dishwasher/status", "1");
+          client.publish("/device/washingmachine/status", "1");
+          //sleep.sleep(5);
+          console.log("Dishwasher and Washingmachine done");
+          client.publish("/device/dishwasher/feedback", "0");
+          client.publish("/device/washingmachine/feedback", "0");
+          client.publish("/device/washingmachine/status", "0");
+          client.publish("/device/dishwasher/status", "0");
+          ready.washingmachine = false;
+          ready.dishwasher = false;
+        }
+      );
+    } else if (status === 1) {
+      if(ready.dishwasher === true){
+        var child;
+        child = exec(
+        "sshpass -p 'ZH3d3IpK' ssh pi@192.168.23.120 'python dishwasher.py'",
+        function(error, stdout, stderr) {
+          if (error !== null) {
+            console.log("exec error: " + error);
+          }
+          //client.publish("/device/status", "1");
+          client.publish("/device/dishwasher/status", "1");
+          //sleep.sleep(5);
+          ready.dishwasher = false;
+          console.log("Dishwasher done");
+          client.publish("/device/dishwasher/feedback", "0");
+          client.publish("/device/dishwasher/status", "0");
+          //done.dishwasher = true;
+        }
+      );
       }
-      console.log("Dishwasher and Washingmachine done");
-      client.publish("/device/dishwasher/feedback", "0");
-      client.publish("/device/washingmachine/feedback", "0");
-      ready.washingmachine=false;
-      ready.dishwasher=false;
-
-    
-    });
+      else if (ready.washingmachine === true){
+      var child;
+      child = exec(
+      "sshpass -p 'ZH3d3IpK' ssh pi@192.168.23.120 'python washingmachine.py'",
+      function(error, stdout, stderr) {
+        if (error !== null) {
+          console.log("exec error: " + error);
+        }
+        //client.publish("/device/status", "3");
+        client.publish("/device/washingmachine/status", "1");
+        //sleep.sleep(5);
+        ready.washingmachine = false;
+        console.log("Washingmachine done");
+        client.publish("/device/washingmachine/feedback", "0");
+        client.publish("/device/washingmachine/status", "0");
+      }
+    );
     }
-    else if (status===1){
-      var child;
-      child = exec("sshpass -p 'ZH3d3IpK' ssh pi@192.168.23.120 'python dishwasher.py'", function(error, stdout, stderr) {
-      if (error !== null) {
-        console.log("exec error: " + error);
-      }
-      console.log("Dishwasher done");
-      client.publish("/device/dishwasher/feedback", "0");
-      ready.dishwasher=false;
       
-    });
-       
+    }
   }
-}
-
-  
 
 });
